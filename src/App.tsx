c068728508006f58dc9d762c5a6737698c666bed
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2023-10-30 14:12:53 +0200

    ref: di fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import { TasksList } from "./tasks/ui/tasks-list";
+import {TasksList} from "./tasks/ui/tasks-list";
+import {CreateTaskForm} from "./tasks/ui/create-task-from.tsx";
+import {useTasks} from "./tasks/model/use-tasks.ts";
+import {TaskItem} from "./tasks/ui/task-item.tsx";
+import {UserSelect} from "./user/ui/user-select.tsx";
 
 export function App() {
+  const {addTask, tasks, toggleCheckTask, updateOwner, removeTask} = useTasks();
+
   return (
     <>
-      <TasksList />
+      <CreateTaskForm onCreate={addTask}/>
+      <TasksList
+        tasks={tasks}
+        renderTask={(task) => (
+          <TaskItem
+            key={task.id}
+            done={task.done}
+            title={task.title}
+            onToggleDone={() => toggleCheckTask(task.id)}
+            onDelete={() => removeTask(task.id)}
+            userSelectSlot={
+              <UserSelect userId={task.ownerId} onChangeUserId={(ownerId) => updateOwner(task.id, ownerId)}/>
+            }
+          />
+        )}
+      />
     </>
   );
 }
+
